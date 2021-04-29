af3be9f44a38dc788e1addd3e7a7ffdee21dfe35
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-28 21:03:24 -0400

    Added questions and generateHTML File

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHTML = require('./generateHTML');
 
 
 const promptUser = () => {
@@ -8,25 +9,29 @@ const promptUser = () => {
             type: 'input',
             name: 'name',
             message: 'What is your name?',
+            validate: (answer)=>{ if(answer){return true} else {return 'Please enter an answer before proceeding'}}
           },
           {
             type: 'input',
             name: 'position',
             message: 'What is your position?',
+            validate: (answer)=>{ if(answer){return true} else {return 'Please enter an answer before proceeding'}}
           },
           {
             type: 'input',
             name: 'email',
             message: 'What is your email?',
+            validate: (answer)=>{ if(answer){return true} else {return 'Please enter an answer before proceeding'}}
           },
           {
             type: 'input',
             name: 'name',
             message: 'What is your github username?',
+            validate: (answer)=>{ if(answer){return true} else {return 'Please enter an answer before proceeding'}}
           },
     ]).then((answers) => {
         const filename =  `${answers.name.toLowerCase().split(' ').join('')}.html`;
-        fs.writeFile(filename, promptUser(answers), (err) =>
+        fs.writeFile(filename, generateHTML(answers), (err) =>
         err? console.log(err) : console.log("README has been created! Check it out!"))
     
     })
