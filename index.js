1d335d86ba26f90e2211a1fe1c2ba2e38d8cf888
stephan du Plooy <sdpphotography1@gmail.com>
2021-04-28 20:53:14 -0400

    Added questions

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+          },
+          {
+            type: 'input',
+            name: 'position',
+            message: 'What is your position?',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is your github username?',
+          },
+    ]).then((answers) => {
+        const filename =  `${answers.name.toLowerCase().split(' ').join('')}.html`;
+        fs.writeFile(filename, promptUser(answers), (err) =>
+        err? console.log(err) : console.log("README has been created! Check it out!"))
+    
+    })
+    };
+    
+    promptUser()
