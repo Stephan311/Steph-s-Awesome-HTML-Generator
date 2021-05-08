4b1b942a92ea2e6dc4e3eef5e823e742018e8767
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-07 21:23:15 -0400

    added a map function to the generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,29 +1,33 @@
 
 
-function generateHTML(answer) {
-    return `<!DOCTYPE html>
+
+function generateHTML(answers) {
+    `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
       <title>Document</title>
-    </head>
-    <body>
-      <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Hi! My name is ${answer.name}</h1>
-        <p class="lead">I am from ${answer.Position}.</p>
-        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-          <li class="list-group-item">My GitHub username is ${answer.github}</li>
-          <li class="list-group-item">LinkedIn: ${answer.linkedin}</li>
-        </ul>
+    </head>`
+    const generated = answers.map(answer => (
+    `<body>
+    <div class="container">
+    <div class="row align-items-start">
+      <div class="col">
+        <h3>${answer.managerP}</h3>
+        <p>${answer.name}</p>
+        <p>${answer.ID}</p>
+        <p>${answer.Email}</p>
       </div>
-    </div>
+  </div>
+  </div>
     </body>
     </html>`
-
+  ));
+  return generated.join("");
 }
+//return all of the templates generated
+//use a join 
 
 module.exports = generateHTML;
